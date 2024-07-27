77a39e08ba2af7717475d143c616699586ed2db9
Daniella <daniellaelbaz711@gmail.com>
2024-07-27 23:38:33 +0300

    chanse sintax

--- js/madaHomePage.js
+++ js/madaHomePage.js
@@ -45,19 +45,18 @@ async function getAllNotification() {
 
             result.eventNotification.forEach(notification => {
                 const dateDiv = document.createElement('div');
-                dateDiv.textContent = `Date: ${new Date(notification.date).toLocaleDateString()}`;
+                dateDiv.textContent =` ${new Date(notification.date).toLocaleDateString()}`;
                 dateElement.appendChild(dateDiv);
 
                 const eventDiv = document.createElement('div');
-                eventDiv.textContent = `Event: ${notification.notfication}`;
+                eventDiv.classList.add('eventDiv');
+                eventDiv.innerHTML =` <span class="bullet-point">•</span> ${notification.notfication}`;
                 highPossibilityEventsElement.appendChild(eventDiv);
 
                 const mapDiv = document.createElement('div');
                 mapDiv.innerHTML = `<img src="${notification.day_map}" alt="Day Map">`;
                 mapPointsElement.appendChild(mapDiv);
             });
-
-            console.log('Day details:', { dateElement, highPossibilityEventsElement, mapPointsElement });
         } else {
             alert(result.message || 'Load details failed');
         }
