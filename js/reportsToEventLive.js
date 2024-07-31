be5559ca546cff98c1c0dee1ed4c7598775f4fed
eitanBellaiche <eitanBellaiche@gmail.com>
2024-07-31 15:34:34 +0300

    update reportTOeventLive js

--- js/reportsToEventLive.js
+++ js/reportsToEventLive.js
@@ -23,22 +23,27 @@ async function fetchEventData() {
 function displayEvents(events) {
     const eventContainer = document.getElementById('eventContainer');
     eventContainer.innerHTML = ''; // Clear previous content if any
+
     events.forEach(event => {
         const eventElement = document.createElement('div');
         eventElement.className = 'event';
-        
+
         const eventName = document.createElement('p');
         eventName.textContent = `שם אירוע: ${event.event_name}`;
-        eventElement.appendChild(eventName);
-        
+        eventName.className = 'text-item'; // הוספת המחלקה text-item
+
         const eventStatus = document.createElement('p');
         eventStatus.textContent = `סטטוס אירוע: ${event.event_status}`;
-        eventElement.appendChild(eventStatus);
-        
+        eventStatus.className = 'text-item'; // הוספת המחלקה text-item
+
         const eventType = document.createElement('p');
         eventType.textContent = `סוג אירוע: ${event.type_event}`;
+        eventType.className = 'text-item'; // הוספת המחלקה text-item
+
+        eventElement.appendChild(eventName);
+        eventElement.appendChild(eventStatus);
         eventElement.appendChild(eventType);
-        
+
         eventContainer.appendChild(eventElement);
     });
 }
